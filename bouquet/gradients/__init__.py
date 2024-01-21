52a7fbe58d69ddd3b00c5da961343599e2d36f37
Mak Sim <mmmakkkss@proton.me>
2024-01-21 18:51:47 +0300

    Update bouquet.gradients module

--- bouquet/gradients/__init__.py
+++ bouquet/gradients/__init__.py
@@ -1,7 +1,19 @@
 '''
 Gradients
 =========
+
+This module provides various types of gradients. Currently, 
+only linear and radial gradients are supported. Other 
+types of gradients will be added later.
+
+.. note::
+
+    Note that you should import `bouquet.gradients` module
+    in Python script to use gradients in KvLang.
+
 '''
 
 from .linear import *
 from .radial import *
+
+__all__ = ('LinearGradient', 'RadialGradient', )
