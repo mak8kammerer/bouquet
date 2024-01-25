12eb09b9677e10b2907148a4353630d8ffb38187
Mak Sim <mmmakkkss@proton.me>
2024-01-25 22:06:32 +0300

    Add description

--- bouquet/__init__.py
+++ bouquet/__init__.py
@@ -1,3 +1,13 @@
+'''
+Bouquet
+=======
+
+Welcome! Bouquet library is collection of awesome widgets and helpful tools 
+for Kivy framework. Using it, you can build modern and beautiful UI for 
+your applications. Enjoy!
+
+'''
+
 from kivy.logger import Logger
 
 from ._version import __version__
