3678b034f7c06f712ba19406b5fc04cfb01537a6
Mak Sim <mmmakkkss@proton.me>
2024-04-11 14:49:02 +0300

    Tests - init

---
+++ tests/test_gradients.py
@@ -0,0 +1,64 @@
+from kivy.tests.common import GraphicUnitTest
+
+
+class GradientsTests(GraphicUnitTest):
+
+    def test_linear_gradient(self):
+        from bouquet.gradients import LinearGradient
+
+        render = self.render
+
+        wid = LinearGradient()
+        render(wid)
+
+        wid.bottom_left_color = '#ff0000'
+        render(wid)
+
+        texture = LinearGradient.render_texture()
+        self.assertEqual(texture.size, (100, 100))
+
+        tex_size = (500, 600)
+        texture = LinearGradient.render_texture(
+            size=tex_size,
+            bottom_left_color='#00000000'
+        )
+        self.assertEqual(texture.size, tex_size)
+
+        pixels = texture.pixels
+        self.assertEqual(len(pixels), tex_size[0] * tex_size[1] * 4)
+
+        # bottom left corner -> transparent black
+        self.assertEqual(pixels[:4], b'\x00\x00\x00\x00')
+        # top right corner -> yellow
+        self.assertEqual(pixels[-4:], b'\xff\xff\x00\xff')
+
+    def test_radial_gradient(self):
+        from kivy.graphics.fbo import Fbo
+        from bouquet.gradients import RadialGradient
+
+        render = self.render
+
+        wid = RadialGradient()
+        render(wid)
+
+        wid.border_color = '#ff0000'
+        render(wid)
+
+        texture = RadialGradient.render_texture()
+        self.assertEqual(texture.size, (100, 100))
+
+        tex_size = (500, 500)
+        texture = RadialGradient.render_texture(
+            size=tex_size,
+            border_color='#00000000'
+        )
+        self.assertEqual(texture.size, tex_size)
+
+        texture.save('image.png')
+
+        pixels = texture.pixels
+        self.assertEqual(len(pixels), tex_size[0] * tex_size[1] * 4)
+
+        # bottom left corner -> transparent black
+        self.assertEqual(pixels[:4], b'\x00\x00\x00\x00')
+        # TODO: test center_color here
